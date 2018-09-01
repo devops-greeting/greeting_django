dd0f2eef123347515fbc6b20125c53d9df083c04
nguyenhoangnhon <42534013+nguyenhoangnhon@users.noreply.github.com>
2018-09-01 23:19:46 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd /var/lib/jenkins/workspace/${jobname}/src/mysite && ls
-# python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
 
-# sleep 5
+sleep 5
+
+exit
 
 
