1a850965e6ac5b5cab9438b20397897fb8fcaff4
nguyenhoangnhon <42534013+nguyenhoangnhon@users.noreply.github.com>
2018-09-02 14:03:01 +0700

    Update views.py

--- src/mysite/polls/views.py
+++ src/mysite/polls/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from os
+print (os.environ['ten'])
 
 def index(request):
-    return HttpResponse("Hello, %s" %ten)
+    return HttpResponse("Hello, Nhon")
