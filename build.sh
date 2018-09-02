6fae6fffb56cebbcaf4abc1bb176e9ddff3c7efb
nguyenhoangnhon <42534013+nguyenhoangnhon@users.noreply.github.com>
2018-09-02 14:48:49 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 printenv ten
 cd /var/lib/jenkins/workspace/${jobname}/src/mysite
-${BUILD_ID}=dontKILLME nohup python3 manage.py runserver 0.0.0.0:8000 
+python3 manage.py runserver 0.0.0.0:8000 
 
 
 
