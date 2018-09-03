04ec637f9790990810bf5f09ee2a69b504876040
nguyenhoangnhon <42534013+nguyenhoangnhon@users.noreply.github.com>
2018-09-03 15:34:41 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 printenv ten
 cd /var/lib/jenkins/workspace/${jobname}/src/mysite
-python3 manage.py runserver 0.0.0.0:8000 
+nohup python3 manage.py runserver 0.0.0.0:8000 &
 
-sleep 1
-kill -INT $PPID
+
 
 
 
